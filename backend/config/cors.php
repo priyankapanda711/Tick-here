11fbdbd1ebab0f0b322816f809673e0b57c7d164
supriyarani-dhal <supriyadhal50@gmail.com>
2025-06-11 16:44:50 +0530

    adding cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true, //must be true for sanctum authentication to work
 
 ];
